7d42fd3583b7e1b18f457d2beb38fb8fa9bed527
shmuelhizmi <shmuelhizmi1@gmail.com>
2020-06-05 18:31:56 +0300

    branding

--- samples/sample.ts
+++ samples/sample.ts
@@ -1,33 +1,34 @@
-import * as vscode from 'vscode';
-import {CanIUse} from './can-i-use';
+import * as vscode from "vscode";
+import { CanIUse } from "./can-i-use";
 
 export function activate(context: vscode.ExtensionContext) {
-    var disposable = vscode.commands.registerCommand('extension.canIUse', () => {
-        let caniuse = new CanIUse();
+  var disposable = vscode.commands.registerCommand("extension.canIUse", () => {
+    let caniuse = new CanIUse();
 
-        const editor = vscode.window.activeTextEditor;
-        if (!editor) {
-            return;
-        }
-        var expandedSelection = undefined;
-        expandedSelection = getSelection(editor);
-        if (expandedSelection) {
-            var word = editor.document.getText(expandedSelection);
-            if (word) {
-                caniuse.retrieveInformation(caniuse.getNormalizedRule(word).toLowerCase(), showOutput);
-            }
-        }
-    });
+    const editor = vscode.window.activeTextEditor;
+    if (!editor) {
+      return;
+    }
+    var expandedSelection = undefined;
+    expandedSelection = getSelection(editor);
+    if (expandedSelection) {
+      var word = editor.document.getText(expandedSelection);
+      if (word) {
+        caniuse.retrieveInformation(
+          caniuse.getNormalizedRule(word).toLowerCase(),
+          showOutput
+        );
+      }
+    }
+  });
 
-    context.subscriptions.push(disposable);
+  context.subscriptions.push(disposable);
 }
 
+(function (angular) {
+  "use strict";
 
-(function(angular) {
-  'use strict';
-
-  angular.module('chrome')
-    .factory('historyService', historyService);
+  angular.module("chrome").factory("historyService", historyService);
 
   function historyService($q, sharedService) {
     return {
@@ -42,37 +43,34 @@ export function activate(context: vscode.ExtensionContext) {
       chromeParam.startTime = param.startDate;
       chromeParam.maxResults = param.maxResults;
       var deferred = $q.defer();
-      chrome.history.search(chromeParam,
-        function(response) {
-          var list = sharedService.populateList(response);
-          deferred.resolve(list);
-        });
+      chrome.history.search(chromeParam, function (response) {
+        var list = sharedService.populateList(response);
+        deferred.resolve(list);
+      });
       return deferred.promise;
     }
 
     function getVisits(url) {
       var deferred = $q.defer();
-      chrome.history.getVisits({ url: url }, function(visits) {
+      chrome.history.getVisits({ url: url }, function (visits) {
         deferred.resolve(visits);
       });
       return deferred.promise;
     }
 
     function monitorHistory(addHistoryCB) {
-      chrome.history.onVisited.addListener(function(historyItem) {
+      chrome.history.onVisited.addListener(function (historyItem) {
         addHistoryCB(historyItem);
       });
     }
   }
-
 })(angular);
 
-
 const options = {
-	method: 'GET',
-	uri: 'https://api.github.com/com/search/repositores',
-	headers: {
-		'User-Agent': 'gittr-cli',
-	},
-	qs: { q: `language:${argv.lang}`, sort: 'stars'},
+  method: "GET",
+  uri: "https://api.github.com/com/search/repositores",
+  headers: {
+    "User-Agent": "gittr-cli",
+  },
+  qs: { q: `language:${argv.lang}`, sort: "stars" },
 };
